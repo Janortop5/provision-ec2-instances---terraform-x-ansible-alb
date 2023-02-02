d4e70321ccfd342979069cadcd3c09a47be07f07
Aladejana Emmanuel <dejanaemzy37@gmail.com>
2023-02-02 08:56:14 +0100

    added more comments

--- altschool-terraform-mini-project/main.tf
+++ altschool-terraform-mini-project/main.tf
@@ -1,6 +1,7 @@
-#When the configuration is running, Immediately the hosted zone has been created
-#Add the name servers for the hosted zone to your domain provider dashboard to ensure the configuration
-#runs successfully
+#When the configuration is running, immediately the hosted zone has been created
+#add the name servers for the hosted zone to your domain provider dashboard to ensure the configuration
+#runs successfully, this is needed for the acm certificate to be validated.
+#If the configuration fails, make sure to add the hosted zone name servers to your domain provider's dashboard and apply#again, that's if you didn't do it when the configuration was running.
 
 module "ec2" {
   source = "../modules/ec2"
