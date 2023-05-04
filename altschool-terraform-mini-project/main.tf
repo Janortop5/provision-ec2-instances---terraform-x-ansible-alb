cb216475ed3aaa16083f7cb7ea8cb00078a0d029
Aladejana, Emmanuel Abiodun <dejanaemzy37@gmail.com>
2023-05-04 16:27:56 +0100

    Update main.tf

--- altschool-terraform-mini-project/main.tf
+++ altschool-terraform-mini-project/main.tf
@@ -1,9 +1,3 @@
-#When the configuration is running, immediately the hosted zone has been created
-#add the name servers for the hosted zone to your domain provider dashboard to ensure the configuration
-#runs successfully, this is needed for the acm certificate to be validated.
-#If the configuration fails, make sure to add the hosted zone name servers to your domain provider's dashboard and apply
-#again, that's if you didn't do it when the configuration was running.
-
 module "ec2" {
   source = "../modules/ec2"
   host_inventory = {
@@ -24,6 +18,12 @@ resource "null_resource" "ansible" {
   ]
 }
 
-output "instruction" {
-  value = "If the configuration failed, make sure to add aws name servers for your hosted zone to your domain provider's dashboard\nThat's if you didn't do it when the configuration was running"
+  terraform {
+   backend "s3" {
+   region         = "us-east-1"
+   bucket         = "altschool-tf-state"
+   key            = "exam/terraform.tfstate"
+   dynamodb_table = "terraform-state-lock"
+   encrypt        = true
+ }
 }
